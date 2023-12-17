06beecfb6af59d0c4afc58ed49fdf16485e50118
Lucas Mancini <mancini.lucas@gmail.com>
2023-12-17 19:18:18 +1100

    Add Vercel analytics and webVitals monitoring.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from "astro/config";
-// import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
+
 export default defineConfig({
-  // output: "static",
-  // adapter: vercel({
-  //   webAnalytics: {
-  //     enabled: true,
-  //   },
-  // }),
-});
+  output: "static",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    speedInsights: {
+      enabled: true,
+    },
+  }),
+);
